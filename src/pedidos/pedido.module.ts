509a2d9791a9ee4d50f6c2bc3d98308fd9661560
SakuraNeeko <156485248+SakuraNeeko@users.noreply.github.com>
2024-03-21 16:44:54 -0500

    Update pedido.module.ts

--- src/pedidos/pedido.module.ts
+++ src/pedidos/pedido.module.ts
@@ -1,4 +1,3 @@
-// src/pedidos/pedido.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PedidoSchema } from './schemas/pedido.schema';
